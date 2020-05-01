e7de0154c72aa1a70844109dde0c43764f61fc21
Adrien Roques <adrien.roques.31@outlook.fr>
2020-05-01 13:00:23 +0200

    Change background

--- client/src/app/about/carousel/carousel.component.ts
+++ client/src/app/about/carousel/carousel.component.ts
@@ -14,8 +14,8 @@ export class CarouselComponent {
 }
 
 const IMAGES: Image[] = [
-  { 'title': 'Work easy, work faster', 'url': '../../assets/img/carousel_1.jpg' },
   { 'title': 'Real time IA edidemic prediction', 'url': '../../assets/img/carousel_2.jpg' },
+  { 'title': 'Work easy, work faster', 'url': '../../assets/img/carousel_1.jpg' },
   { 'title': 'Keep your notes around the world', 'url': '../../assets/img/carousel_3.jpg' },
   { 'title': 'Secure stored HDS Datas ', 'url': '../../assets/img/carousel_4.jpg' },
   { 'title': '100% made in France by the French Tech ', 'url': '../../assets/img/carousel_5.jpg' }
